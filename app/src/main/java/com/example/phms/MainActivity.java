ec261e0ec8c05784c39221cb114dc5be864beb11
Smarika12 <115603500+Smarika12@users.noreply.github.com>
2023-04-25 22:59:16 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/phms/MainActivity.java
+++ app/src/main/java/com/example/phms/MainActivity.java
@@ -11,12 +11,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     Button mSubmit,mCancel,mExit,mSearch;
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     EditText mBPText;
     EditText mSugarText;
     EditText mBGText;
+    TextView mStatusText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         mBPText   = (EditText)findViewById(R.id.bloodPressure);
         mSugarText   = (EditText)findViewById(R.id.sugarLevel);
         mBGText = (EditText)findViewById(R.id.bgTextName);
+        mStatusText = (TextView)findViewById(R.id.statusTextView);
         mSubmit.setOnClickListener(
                 new View.OnClickListener()
                 {
@@ -80,12 +85,23 @@ public class MainActivity extends AppCompatActivity {
                                     @Override
                                     public void onSuccess(DocumentReference documentReference) {
                                         Log.d("Smarika", "DocumentSnapshot written with ID: " + documentReference.getId());
+                                        mSubmit.setEnabled(false);
+                                        mBGText.getText().clear();
+                                        mBPText.getText().clear();
+                                        mHeightText.getText().clear();
+                                        mWeightText.getText().clear();
+                                        mSugarText.getText().clear();
+                                        mStatusText.setText("Sucessfully submitted!");
+                                        mSubmit.setEnabled(true);
                                     }
                                 })
                                 .addOnFailureListener(new OnFailureListener() {
                                     @Override
                                     public void onFailure(@NonNull Exception e) {
                                         Log.w("Smarika", "Error adding document", e);
+                                        mSubmit.setEnabled(false);
+                                        mStatusText.setText("Error submitting the form!");
+                                        mSubmit.setEnabled(true);
                                     }
                                 });;
                     }
